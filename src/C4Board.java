43fa70e81879629398b9fd1009bab95112c0a705
Jürgen Geyer <S2510454012@fhooe.at>
2026-05-04 19:51:29 +0200

    Add column heuristic

--- src/C4Board.java
+++ src/C4Board.java
@@ -57,9 +57,11 @@ public final class C4Board implements Board {
     firstPlayerMoves = new Move[NUMBER_OF_COLUMNS];
     secondPlayerMoves = new Move[NUMBER_OF_COLUMNS];
 
+    int[] moveOrder = {3, 4, 2, 5, 1, 6, 0, 7}; // Works for both 7 and 8 columns
+
     for (int i = 0; i < NUMBER_OF_COLUMNS; i++) {
-      firstPlayerMoves[i] = new C4Move(firstPlayer, i);
-      secondPlayerMoves[i] = new C4Move(secondPlayer, i);
+      firstPlayerMoves[i] = new C4Move(firstPlayer, moveOrder[i]);
+      secondPlayerMoves[i] = new C4Move(secondPlayer, moveOrder[i]);
     }
 
   }// end constructor
